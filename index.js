7c8280e49491fad67df1f16ccbed5dd459d54c2a
Marco Nardone <39136442+CodingPeaks@users.noreply.github.com>
2023-07-12 20:18:21 +0200

    Live555 download and installation procedure
    The code has to be refined and optimized, there are a lot of redundant code blocks but the functionality is there. 
    
    TODO: Motion configuration file initialization
    I'll have to change one or two lines inside motion cfg file to create a directory where RTSP sources cfg files (IP Cams) will be stored.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { execSync } = require('child_process');
+const { exit } = require('process');
 
 console.log("Motion Live555 Autoconf")
 
@@ -20,7 +21,7 @@ function runCommand(commandString) {
     return result;
 };
 
-function installPackage(packageName) {
+function installPackage(packageName, checkExecutable = packageName) {
     var result = { installed: false, checked: false };
     console.log(`Installing package '${packageName}'...`)
     let commandString = `apt -y install ${packageName}`;
@@ -28,17 +29,18 @@ function installPackage(packageName) {
     var installResult = runCommand(commandString);
     if (installResult.code == 0) {
         result.installed = true;
-        let checkInstallResult = checkPackageInstallation(packageName);
+        let checkInstallResult = checkPackageInstallation(checkExecutable);
         if (checkInstallResult.code == 0) {
             result.checked = true;
-        }else{
+        } else {
             console.log(`${packageName} installation check failed`);
         }
-    }else{
+    } else {
         console.log(`${packageName} installation exited with error code ${installResult.code}`)
     }
 
-    return result;
+    checkInstallOp(packageName, result)
+
 }
 
 function checkPackageInstallation(packageName) {
@@ -56,11 +58,84 @@ function createDirectories() {
     });
 }
 
-/*********************************************/
+function checkInstallOp(target, ec) {
+    if (ec.installed && ec.checked) {
+        console.log(`${target} successfully installed!`);
+    } else {
+        console.log(`There was an error during the installation of ${target}:`)
+        console.log(ec);
+        exit(1);
+    }
+}
+
+/******************REPO PACKAGES***********************/
 
 createDirectories();
 
-let motion = installPackage("motion");
-if(motion.installed && motion.checked){
-    console.log("Motion successfully installed!");
+installPackage("motion");
+installPackage("git");
+installPackage("build-essential", "make");
+installPackage("libssl-dev", "openssl");
+
+/******************NON-REPO PACKAGES***********************/
+
+let live555Downloaded = runCommand(`ls /tmp/live555`);
+let ghRepo = "https://github.com/rgaufman/live555.git";
+
+if (live555Downloaded.code != 0) { //if the directory doesn't exist
+    console.log(`Cloning live555 from the official Github repo...[${ghRepo}]`)
+    let live555Clone = runCommand(`cd /tmp && git clone ${ghRepo}`);
+
+    if (live555Clone.code != 0) {
+        console.log(`There was an error during the clone operation of Live555:`)
+        console.log(live555Clone);
+        exit(1);
+    }else{
+        console.log(`Successfully cloned Live555 from official repo`)
+    }
+
+} else {
+    console.log(`Live555 directory found in /tmp, presuming it's our repo...[${ghRepo}]`);
+}
+
+console.log("Generating make files for live555...")
+let genMakefiles = runCommand(`cd /tmp/live555 && ./genMakefiles linux`);
+
+if (genMakefiles.code != 0) {
+    console.log(`There was an error during the generation of make files for Live555:`)
+    console.log(genMakefiles);
+    exit(1);
+}else{
+    console.log(`Successfully generated Live555 make files`)
+}
+
+console.log("Compiling...")
+let live555make = runCommand(`cd /tmp/live555 && make -j4`);
+
+if (live555make.code != 0) {
+    console.log(`There was an error during the compile operation of Live555:`)
+    console.log(live555make);
+    exit(1);
+}else{
+    console.log(`Successfully compiled Live555 binaries`)
+}
+
+console.log("Extracting live555ProxyServer binary to /usr/bin/ and making it executable...")
+let live555ProxyExtractAndX = runCommand(`cd /tmp/live555/proxyServer && \
+                                      chmod +x live555ProxyServer && \
+                                      cp live555ProxyServer /usr/bin/`);
+
+if (live555ProxyExtractAndX.code != 0) {
+    console.log(`There was an error during the copy or chmod of Live555ProxyServer executable to /usr/bin/:`)
+    console.log(live555ProxyExtractAndX);
+    exit(1);
+}else{
+    console.log(`live555ProxyServer binary extracted, permissions have been set`)
+}
+
+let checkLive555InstallResult = checkPackageInstallation("live555ProxyServer");
+if (checkLive555InstallResult.code == 0) {
+    console.log(`live555ProxyServer has been installed successfully`);
+} else {
+    console.log(`live555ProxyServer installation has failed, binaries not found in /usr/bin/`);
 }
